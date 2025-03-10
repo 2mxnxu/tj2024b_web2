d3e10373faeb4671242664e14e2dbe29aea89437
2ãmxnxu <leeeun099@naver.com>
2025-03-10 12:11:28 +0900

    리엑트 게시물 전체 출력

--- src/main/java/example/day07/controller/ReactController.java
+++ src/main/java/example/day07/controller/ReactController.java
@@ -1,24 +1,32 @@
 package example.day07.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-@RestController
-@RequestMapping("/day07/react") // 공동 URL
-@CrossOrigin("http://localhost:5173") // CORS 정책 허용, 특정 url만 허용 가능하도록 한다
-//@CrossOrigin("http://192.168.40.31:5173") // CORS 정책 허용, 특정 url만 허용 가능하도록 한다
-
+@RestController // 빈 등록
+@RequestMapping("/day07/react") // 공통 URL
+@CrossOrigin("http://localhost:5173") // CORS 정책 허용 , 특정 url만 허용 가능하도록 한다. , 우리의 리액트 서버 허용
 public class ReactController {
-    // [1] post
-    @RequestMapping("")
-    public boolean onPost(@RequestBody Map<String, Object> map) {
-        System.out.println("React Controller onPost");
-        System.out.println("map" + map);
+    // (샘플데이터 저장하는 리스트 == 데이터베이스 대신에 사용.)
+    private List<Map<Object,String>> boards = new ArrayList<>();
+    // [1] POST
+    @PostMapping("")
+    // [POST] http://localhost:8080/day07/react
+    public boolean onPost(@RequestBody Map<Object,String> map ){
+        System.out.println("ReactController.onPost");
+        System.out.println("map = " + map);// soump
+        boards.add( map ); // 요청받은 데이터를 리스트에 저장
         return true;
-    }
+    } // f end
 
+    // [2] GET
+    @GetMapping("")
+    // [GET] http://localhost:8080/day07/react
+    public List<Map<Object,String>> onFindAll(){
+        System.out.println("ReactController.onFindAll");
+        return boards; // 리스트를 응답하기
+    }
 }
