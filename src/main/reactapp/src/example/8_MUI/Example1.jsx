9183b38f11b742f33a21c8accb3765ea48ef5e43
2ãmxnxu <leeeun099@naver.com>
2025-03-13 14:36:04 +0900

    MUI 사용

--- src/main/reactapp/src/example/8_MUI/Example1.jsx
+++ src/main/reactapp/src/example/8_MUI/Example1.jsx
@@ -1,10 +1,17 @@
+import * as React from 'react';
 import Button from '@mui/material/Button';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
+import Modal from '@mui/joy/Modal';
+import ModalClose from '@mui/joy/ModalClose';
+import Typography from '@mui/joy/Typography';
+import Sheet from '@mui/joy/Sheet';
+import { BarChart } from '@mui/x-charts/BarChart';
 
 export default function Example1() {
+    const [open, setOpen] = React.useState(false);
     return(<>
         <Button variant= "outlined">Outlined</Button>
 
@@ -20,5 +27,44 @@ export default function Example1() {
           <MenuItem value={30}>Thirty</MenuItem>
         </Select>
       </FormControl>
+
+      <Button variant="outlined" color="neutral" onClick={() => setOpen(true)}>
+        모달창 열기
+      </Button>
+
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
+        <Sheet
+          variant="outlined"
+          sx={{ maxWidth: 500, borderRadius: 'md', p: 3, boxShadow: 'lg' }}
+        >
+          <ModalClose variant="plain" sx={{ m: 1 }} />
+          <Typography
+            component="h2"
+            id="modal-title"
+            level="h4"
+            textColor="inherit"
+            sx={{ fontWeight: 'lg', mb: 1 }}
+          >
+            This is the modal title
+          </Typography>
+          <Typography id="modal-desc" textColor="text.tertiary">
+            Make sure to use <code>aria-labelledby</code> on the modal dialog with an
+            optional <code>aria-describedby</code> attribute.
+          </Typography>
+        </Sheet>
+      </Modal>
+
+      <BarChart
+      xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+      series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+      width={500}
+      height={300}
+    />
     </>)
 }
