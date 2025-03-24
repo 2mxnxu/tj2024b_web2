9b0d05bdfe5ad0da774f071e9e60b4a167944a4c
2ãmxnxu <leeeun099@naver.com>
2025-03-24 15:54:48 +0900

    스레드

--- src/main/java/example/day10/_멀티스레드/task.java
+++ src/main/java/example/day10/_멀티스레드/task.java
@@ -32,26 +32,37 @@ public class task {
         Music music = new Music();
         Web web = new Web();
         boolean multi = true;
-        while (multi){
-            Scanner scan = new Scanner( System.in );
+        while (multi) {
+            Scanner scan = new Scanner(System.in);
             System.out.println("1. 음악 On/Off | 2. 웹서핑 On/Off | 0. 종료");
             int i = scan.nextInt();
-
             if (i == 1) {
-                System.out.println("음악 시작!");
-                music.state1 = true;
-                music.start();
-            }else if (i == 2) {
-                System.out.println("웹서핑 시작!");
-                web.state2 = true;
-                web.start();
-            }else{
-                multi = false;
-                System.out.println("종료합니다.");
-                music.state1 = false;
-                web.state2 = false;
+                if (music.state1 == false) {
+                    System.out.println("음악 시작!");
+                    music.start();
+                    music.state1 = true;
+                } else if (music.state1 == true) {
+                    System.out.println("음악 종료!");
+                    music.state1 = false;
+                }
+            } else if (i == 2) {
+                    if (web.state2 == false) {
+                        System.out.println("웹서핑 시작!");
+                        web.start();
+                        web.state2 = true;
+                    } else if (web.state2 == true) {
+                        System.out.println("웹서핑 종료!");
+                        web.state2 = false;
+                    }
+                } else if (i == 0) {
+                    System.out.println("프로그램 종료!");
+                    music.state1 = false;
+                    web.state2 = false;
+                    multi = false;
+                }
             }
         }
+
     }
 
-}
+
